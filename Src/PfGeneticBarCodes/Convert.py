8add89fc953092d3d32a0f95e9121a9a419d5072
Paul Vauterin <paul.vauterin@gmail.com>
2013-07-11 13:54:51 +0100

    Calculate barcode cluster sizes

--- Src/PfGeneticBarCodes/Convert.py
+++ Src/PfGeneticBarCodes/Convert.py
@@ -1,36 +1,204 @@
 from TableUtils import VTTable
 import sys
 
-basepath='/Users/pvaut/Documents/Data/PfGeneticBarcodes'
+#basepath='/Users/pvaut/Documents/Data/PfGeneticBarcodes'
+basepath='/home/pvaut/Documents/Genome/PfGeneticBarcodes'
 
 tb = VTTable.VTTable()
 
-#tb.LoadXls(basepath+'/SurfinsForPV.xlsx','less than 3')
-#tb.SaveFile(basepath+'/snpset1.txt')
-#tb.PrintRows(0,10)
+def ImportData():
+    tb0 = VTTable.VTTable()
+    tb0.LoadXls(basepath+'/SurfinsForPV.xlsx','less than 3')
+    tb0.SaveFile(basepath+'/snpset1.txt')
+    tb0.PrintRows(0,10)
 
-tb.LoadFile(basepath+'/snpset1.txt')
-#tb.PrintInfo()
-tb.PrintRows(0,10)
 
-# Determine groups of snps
-snpgroups=[]
-maxdist = 20000
-lastchromname = ''
-lastposit = -1e9
-for colnr in range(tb.GetColCount()):
-    colname = tb.GetColName(colnr)
-    if colname[0:3] == 'MAL':
-        chromname = colname.split(':')[0]
-        posit=int(colname.split(':')[1])
-        if (chromname != lastchromname) or (posit > lastposit+maxdist):
-            snpgroups.append([])
-        snpgroups[len(snpgroups)-1].append(colnr)
-        lastchromname = chromname
-        lastposit = posit
-        #print(chromname+str(posit))
-print(str(snpgroups))
+def LoadData(tb):
+    tb.LoadFile(basepath+'/snpset1.txt')
+    tb.RenameCol('SnpName','Sample')
+    #tb.PrintRows(0,20)
 
+    # Merge in other sample information
+    infotb = VTTable.VTTable()
+    infotb.LoadXls(basepath+'/Processed_metadata-2.1.xlsx','2.1')
+    infotb.DropCol('Country')
+    infotb.DropCol('Fws')
+    infotb.DropCol('SiteInfoSource')
+    infotb.DropCol('Site')
+    infotb.DropCol('LabSample')
+    infotb.DropCol('LowTypability')
+    infotb.DropCol('PcaOutlier')
+    infotb.DropCol('IsDuplicate')
+    infotb.DropCol('ManualExlusion')
+    infotb.DropCol('Typability')
+    infotb.DropCol('UsedInSnpDiscovery')
+    infotb.DropCol('Notes')
+    #infotb.PrintRows(0, 20)
+    tb2 = VTTable.VTTable()
+    tb2.MergeTablesByKeyFrom(tb,infotb,'Sample',True,False)
+    tb=tb2
+    #tb1.PrintRows(0, 20)
+    #sys.exit()
+
+
+
+    # Determine groups of snps
+    snpcols=[]
+    snpgroups=[]
+    maxdist = 20000
+    lastchromname = ''
+    lastposit = -1e9
+    idx = 0
+    for colnr in range(tb.GetColCount()):
+        colname = tb.GetColName(colnr)
+        if colname[0:3] == 'MAL':
+            snpcols.append(colnr)
+            chromname = colname.split(':')[0]
+            posit=int(colname.split(':')[1])
+            if (chromname != lastchromname) or (posit > lastposit+maxdist):
+                idx += 1
+                snpgroups.append({ 'name': 'grp'+str(idx), 'snps': [], 'codes':{}, 'codecount':0 })
+            snpgroups[len(snpgroups)-1]['snps'].append(colnr)
+            lastchromname = chromname
+            lastposit = posit
+            #print(chromname+str(posit))
+
+
+    print('###########################################\nSNP COLS: '+str(snpcols)+"\n###############################")
+    print('###########################################\nSNP GROUPS: '+str(snpgroups)+"\n###############################")
+
+
+
+def CalcBarcodes():
+    for snpgroup in snpgroups:
+        tb.AddColumn(VTTable.VTColumn(snpgroup['name'],'Value'))
+        tb.FillColumn(snpgroup['name'],None)
+    for rownr in tb.GetRowNrRange():
+        key=tb.GetValue(rownr,0)
+        print('KEY='+key)
+        if len(key)>0:
+            for snpgroup in snpgroups:
+                isComplete = True
+                code =''
+                for snpColNr in snpgroup['snps']:
+                    vl=tb.GetValue(rownr,snpColNr)
+                    if vl==0: isComplete = False
+                    code += str(int(float(vl)))
+                if isComplete:
+                    if code not in snpgroup['codes']:
+                        snpgroup['codecount'] += 1
+                        snpgroup['codes'][code] = snpgroup['codecount']
+                    codenr = snpgroup['codes'][code]
+                    print(code+' '+str(codenr))
+                    tb.SetValue(rownr,tb.GetColNr(snpgroup['name']),codenr)
+
+    tb.PrintRows(0,20)
+    tb.SaveFile(basepath+'/result.txt')
+
+
+def CalcSimilarCount():
+    sampleSnpVals=[]
+    for rownr in tb.GetRowNrRange():
+        sampleSnpVals.append( [ tb.GetValue(rownr,snpColNr) for snpColNr in snpcols ] )
+    snprange=range(len(snpcols))
+    sampleDistances = []
+    for rownr1 in tb.GetRowNrRange():
+        print(str(rownr1))
+        snps1 = sampleSnpVals[rownr1]
+        dists= []
+        for rownr2 in tb.GetRowNrRange():
+            snps2 = sampleSnpVals[rownr2]
+            dst = 0
+            for i in snprange:
+                if snps1[i] != snps2[i]:
+                    dst += 1
+            dists.append(dst)
+        sampleDistances.append(dists)
+
+    for maxdist in range(maxmaxdist):
+        colName = 'DistCount_'+str(maxdist)
+        tb.AddColumn(VTTable.VTColumn(colName,'Value'))
+        colNr = tb.GetColNr(colName)
+        tb.FillColumn(colName,None)
+        for rownr1 in tb.GetRowNrRange():
+            print(str(rownr1))
+            dists = sampleDistances[rownr1]
+            cnt =0
+            for rownr2 in tb.GetRowNrRange():
+                if dists[rownr2] <= maxdist:
+                    cnt += 1
+            tb.SetValue(rownr1, colNr, cnt)
+
+    # Remove snp columns
+    snpcolnames = [tb.GetColName(colnr) for colnr in snpcols]
+    for colname in snpcolnames:
+        tb.DropCol(colname)
+    tb.PrintRows(0, 200000)
+    tb.SaveFile(basepath+'/SimilarCount.txt')
+
+
+def CreateAggregatedSimilarCount():
+    tb = VTTable.VTTable()
+    tb.LoadFile(basepath+'/SimilarCount.txt')
+    tb.PrintRows(0, 20)
+
+    sites = VTTable.VTTable()
+    sites.allColumnsText = True
+    sites.LoadFile(basepath+'/sites.txt')
+    sites.DropCol('GeoCode')
+    sites.DropCol('Country')
+    sites.DropCol('SubCont')
+    sites.ConvertColToValue('Latitude')
+    sites.ConvertColToValue('Longitude')
+
+    tbres = VTTable.VTTable()
+    tbres.AddColumn(VTTable.VTColumn('ID', 'Text'))
+    tbres.FillColumn('ID',None)
+    tbres.AddColumn(VTTable.VTColumn('MaxDist', 'Value'))
+    tbres.FillColumn('MaxDist',None)
+    tbres.AddColumn(VTTable.VTColumn('ClusterSize', 'Value'))
+    tbres.FillColumn('ClusterSize',None)
+    tbres.AddColumn(VTTable.VTColumn('ClusterMemberCount', 'Value'))
+    tbres.FillColumn('ClusterMemberCount',None)
+
+    print('====== sites ============')
+    sites.PrintRows(0, 20)
+
+
+    for rownr in sites.GetRowNrRange():
+        sitecode = sites.GetValue(rownr,sites.GetColNr('ID'))
+        for maxdist in range(maxmaxdist):
+            distcolname='DistCount_'+str(maxdist)
+
+            clustercount=[0 for i in range(10000)]
+            for samplenr in tb.GetRowNrRange():
+                if tb.GetValue(samplenr,tb.GetColNr('SiteCode'))  == sitecode:
+                    clustersize=int(tb.GetValue(samplenr,tb.GetColNr(distcolname)))
+                    clustercount[clustersize] += 1
+            for k in range(len(clustercount)):
+                if clustercount[k]>0:
+                    tbres.AddRowEmpty()
+                    resrownr = tbres.GetRowCount() - 1
+                    tbres.SetValue(resrownr,tbres.GetColNr('ID'),sitecode)
+                    tbres.SetValue(resrownr,tbres.GetColNr('MaxDist'),maxdist)
+                    tbres.SetValue(resrownr,tbres.GetColNr('ClusterSize'),k)
+                    tbres.SetValue(resrownr,tbres.GetColNr('ClusterMemberCount'),clustercount[k])
+    tbres.PrintRows(0,20)
+    tbres.SaveFile(basepath+'/clustermembercount.txt')
+    tbres.SaveSQLCreation(basepath+'/clustermembercount_create.sql','clustermembercount')
+    tbres.SaveSQLDump(basepath+'/clustermembercount_data.sql','clustermembercount')
+
+    sites.SaveFile(basepath+'/clustersites.txt')
+    sites.SaveSQLCreation(basepath+'/clustersites_create.sql','clustersites')
+    sites.SaveSQLDump(basepath+'/clustersites_data.sql','clustersites')
+
+
+maxmaxdist = 5
+
+#LoadData(tb)
+#CalcSimilarCount();
+
+CreateAggregatedSimilarCount()
 
 if False:
     import matplotlib.pyplot as plt
