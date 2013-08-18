bbb29015d7ce571096aa61143f548e6c5f8ffcc0
pvaut <paul.vauterin@gmail.com>
2013-08-18 11:23:01 +0100

    More simulations

--- Src/CrossRecombination/Calc.py
+++ Src/CrossRecombination/Calc.py
@@ -11,20 +11,20 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 
 chromosomeInfo = [
-            { 'id': 'Pf3D7_01_v3', 'len': 0.640851E6 },
-            { 'id': 'Pf3D7_02_v3', 'len': 0.947102E6 },
-            { 'id': 'Pf3D7_03_v3', 'len': 1.067971E6 },
-            { 'id': 'Pf3D7_04_v3', 'len': 1.200490E6 },
-            { 'id': 'Pf3D7_05_v3', 'len': 1.343557E6 },
-            { 'id': 'Pf3D7_06_v3', 'len': 1.418242E6 },
-            { 'id': 'Pf3D7_07_v3', 'len': 1.445207E6 },
-            { 'id': 'Pf3D7_08_v3', 'len': 1.472805E6 },
-            { 'id': 'Pf3D7_09_v3', 'len': 1.541735E6 },
-            { 'id': 'Pf3D7_10_v3', 'len': 1.687656E6 },
-            { 'id': 'Pf3D7_11_v3', 'len': 2.038340E6 },
-            { 'id': 'Pf3D7_12_v3', 'len': 2.271494E6 },
-            { 'id': 'Pf3D7_13_v3', 'len': 2.925236E6 },
-            { 'id': 'Pf3D7_14_v3', 'len': 3.291936E6 }
+            { 'id': 'Pf3D7_01_v3', 'len': int(0.640851E6) },
+            { 'id': 'Pf3D7_02_v3', 'len': int(0.947102E6) },
+            { 'id': 'Pf3D7_03_v3', 'len': int(1.067971E6) },
+            { 'id': 'Pf3D7_04_v3', 'len': int(1.200490E6) },
+            { 'id': 'Pf3D7_05_v3', 'len': int(1.343557E6) },
+            { 'id': 'Pf3D7_06_v3', 'len': int(1.418242E6) },
+            { 'id': 'Pf3D7_07_v3', 'len': int(1.445207E6) },
+            { 'id': 'Pf3D7_08_v3', 'len': int(1.472805E6) },
+            { 'id': 'Pf3D7_09_v3', 'len': int(1.541735E6) },
+            { 'id': 'Pf3D7_10_v3', 'len': int(1.687656E6) },
+            { 'id': 'Pf3D7_11_v3', 'len': int(2.038340E6) },
+            { 'id': 'Pf3D7_12_v3', 'len': int(2.271494E6) },
+            { 'id': 'Pf3D7_13_v3', 'len': int(2.925236E6) },
+            { 'id': 'Pf3D7_14_v3', 'len': int(3.291936E6) }
             ]
 
 chromosomes = [info['id'] for info in chromosomeInfo]
@@ -71,18 +71,61 @@ print('Total events: {0}'.format(len(event_posits)))
 
 #print(str(events['chromoids']))
 
-def SimulateEvents():
+def FindClosest(lst,pos):
+    #print(str(lst))
+    i1 = 0
+    i2 = len(lst)-1
+    while i2 > i1+1:
+        i = int((i1+i2)/2)
+        #print('{0} {1} | {2} {3} | {4}'.format(i1,i2,lst[i1],lst[i2],pos))
+        if lst[i] > pos:
+            i2=i
+        else:
+            i1=i
+    #print('{0} {1}'.format(pos,lst[i]))
+    #sys.exit()
+    if (pos<lst[i1]) or (pos>lst[i2]) or (i2!=i1+1):
+        raise Exception('Impossible!')
+    return int((lst[i1]+lst[i2])/2)
+
+def SimulateEvents(snpposits):
     chromoids = []
     posits = []
+
+    RandomPositions = False
 
     for chromoInfo in chromosomeInfo:
         chromoId=chromoInfo['id']
         avcount=chromoInfo['eventcount']
         simcount = numpy.random.poisson(avcount)
+        snppositsonchromo = snpposits[chromoId]
+
+        if RandomPositions:
+            cntt = int(len(snppositsonchromo))
+            snppositsonchromo = []
+            for i1 in range(cntt):
+                snppositsonchromo.append(int(numpy.random.rand()*chromoInfo['len']))
+
+        #print(str(len(snppositsonchromo)))
+        #print(str(snppositsonchromo))
+        #sys.exit()
+
         #print('avcount = {0} simcount = {1}'.format(avcount,simcount))
         chromoposits = []
-        for simnr in range(simcount):
-            chromoposits.append(int(numpy.random.rand()*chromoInfo['len']))
+        simnr=0
+        while simnr < simcount:
+            if RandomPositions:
+                randomsnpnr = int(numpy.random.rand()*len(snppositsonchromo))
+                randomposit = snppositsonchromo[randomsnpnr]
+            else:
+                randomposit = int(numpy.random.rand()*chromoInfo['len'])
+                if (randomposit>=snppositsonchromo[1]) and (randomposit<=snppositsonchromo[len(snppositsonchromo)-2]):
+                    randomposit=FindClosest(snppositsonchromo,randomposit)
+                else:
+                    randomposit = -1
+            if randomposit >= 0:
+                chromoposits.append(randomposit)
+                simnr += 1
         chromoposits = sorted(chromoposits)
         #print(str(chromoposits))
         for posit in chromoposits:
@@ -145,12 +188,42 @@ def CalcIt(events, winSize):
     }
 
 
+def LoadSnpPositions():
+    print('start loading snps')
+    positsperchromo={}
+    files = ['snpposits_3d7_hb3.txt', 'snpposits_7g8_gb4.txt', 'snpposits_hb3_dd2.txt', 'snpposits_3d7_hb3_cortex.txt', 'snpposits_7g8_gb4_cortex.txt', 'snpposits_hb3_dd2_cortex.txt']
+    for file in files:
+        snps=VTTable.VTTable()
+        snps.allColumnsText=True
+        snps.LoadFile(file)
+        for rownr in snps.GetRowNrRange():
+            st=snps.GetValue(rownr,0)
+            chromid = st.split(':')[0]
+            pos = int(st.split(':')[1])
+            if chromid not in positsperchromo:
+                positsperchromo[chromid]=[]
+            positsperchromo[chromid].append(pos)
+    print('snps loaded')
+    for chromoid in positsperchromo:
+        lst = sorted(positsperchromo[chromoid])
+        nr=0
+        while nr<len(lst)-1:
+            if lst[nr] == lst[nr+1]:
+                del lst[nr]
+            else:
+                nr += 1
+        positsperchromo[chromoid] = lst
+        #print('chromo: {0} snpcount {1}'.format(chromoid,len(positsperchromo[chromoid])))
+    #sys.exit()
+    return positsperchromo
+
 def SimulateStats(winSize):
-    maxcount = 20
+    snpposits = LoadSnpPositions()
+    maxcount = 200
     countsList =[[] for i in range(maxcount)]
-    for simnr in range(500):#!!! should be at least 10000
+    for simnr in range(1000):#!!! should be at least 10000
         print('Simulation {0}'.format(simnr))
-        simevents = SimulateEvents()
+        simevents = SimulateEvents(snpposits)
         #print('Simulated total events: {0}'.format(len(simevents['posits'])))
         rs = CalcIt(simevents,winSize)
         print(str(rs['sizecountscumul']))
@@ -173,11 +246,11 @@ def SimulateStats(winSize):
     return rs
 
 
-winSizeList = [500,1000,2000,5000,10000,20000,50000]
+winSizeList = [10000]
 
 
 # Generate or load the simulated data
-if False:
+if True:
     simuldata={}
     for winSize in winSizeList:
         simuldata[str(winSize)] = SimulateStats(winSize)
@@ -193,43 +266,52 @@ print(str(simuldata))
 
 plotnr = 0;
 fig = plt.figure(1)
-fig.set_size_inches(10,30)
+fig.set_size_inches(15,15)
 
 
 for winSize in winSizeList:
     print('\n\n\nWINDOW SIZE {0}'.format(winSize))
     plotnr += 1
     plt.subplot(len(winSizeList),1,plotnr)
+    plt.title('Window size {0}'.format(winSize))
 
     rs = CalcIt(events,winSize)
-    print('SizecountsCumul: '+str(rs['sizecountscumul']))
-
-
     sizecountscumul = rs['sizecountscumul']
+    print('SizecountsCumul: '+str(sizecountscumul))
+
+    simpts_av = [ simuldata[str(winSize)][nr]['average'] for nr in range(len(sizecountscumul)) ]
+    simpts_q99 = [ simuldata[str(winSize)][nr]['q99'] for nr in range(len(sizecountscumul)) ]
+    simpts_q999 = [ simuldata[str(winSize)][nr]['q999'] for nr in range(len(sizecountscumul)) ]
+
     plt.semilogy(sizecountscumul)
-    plt.semilogy(sizecountscumul,'wo')
+    plt.semilogy(sizecountscumul,'bo')
+
+    for idx in range(len(sizecountscumul)):
+        plt.annotate(
+            '{0}, {1}, {2}'.format(sizecountscumul[idx],simpts_av[idx],simpts_q99[idx]),
+            xy = (idx, sizecountscumul[idx]),
+            xytext = (15, 15),
+            textcoords = 'offset points',
+            size=7,
+#            ha = 'right', va = 'bottom',
+#            bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+            arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0')
+        )
 
 
 
-    if True:
-        simpts = [ simuldata[str(winSize)][nr]['average'] for nr in range(len(sizecountscumul)) ]
-        print(str(simpts))
-        plt.semilogy(simpts,'r-')
-        plt.semilogy(simpts,'wo')
 
-        simpts = [ simuldata[str(winSize)][nr]['q99'] for nr in range(len(sizecountscumul)) ]
-        print(str(simpts))
-        plt.semilogy(simpts,'r--')
-        plt.semilogy(simpts,'wo')
+    plt.semilogy(simpts_av,'r-')
+    plt.semilogy(simpts_av,'wo')
+    plt.semilogy(simpts_q99,'r--')
+    plt.semilogy(simpts_q99,'wo')
+    plt.semilogy(simpts_q999,'r:')
+    plt.semilogy(simpts_q999,'wo')
 
-        simpts = [ simuldata[str(winSize)][nr]['q999'] for nr in range(len(sizecountscumul)) ]
-        print(str(simpts))
-        plt.semilogy(simpts,'r:')
-        plt.semilogy(simpts,'wo')
-
-        plt.xlim(0,12)
+    plt.xlim(0,12)
 
 #plt.ylabel('some numbers')
+plt.savefig('plots/plot1.png')
 plt.show()
 
 sys.exit()
