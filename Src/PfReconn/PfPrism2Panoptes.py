bd87bc2e0de885a4338b3475257786e46c2417c1
Paul Vauterin <paul.vauterin@gmail.com>
2014-05-07 14:01:23 +0100

    PfPrism 2 Panoptes

--- Src/PfReconn/PfPrism2Panoptes.py
+++ Src/PfReconn/PfPrism2Panoptes.py
@@ -11,13 +11,15 @@ DBPASS = '1234'
 BASEDIR = '/home/pvaut/Documents/Genome'
 DB='mm6_pfprism'
 
-def LoadSQLTable(tb, tableName):
+def LoadSQLTable(tb, tableName, maxRowCount=None):
     db = MySQLdb.connect(host=DBSRV, user=DBUSER, passwd=DBPASS, db=DB, charset='utf8')
     cur = db.cursor()
     whereclause='select {colnames} from {tablename}'.format(
         colnames=', '.join(['`'+vl+'`' for vl in tb.GetColList()]),
         tablename=tableName
     )
+    if maxRowCount is not None:
+        whereclause += ' LIMIT '+str(maxRowCount)
     print(whereclause)
     cur.execute(whereclause)
     for row in cur.fetchall():
@@ -107,3 +109,117 @@ tbSamples.PrintRows(0, 10)
 
 tbSamples.saveheadertype = False
 tbSamples.SaveFile(BASEDIR+'/SourceData/datasets/PfReconn/datatables/samples/data')
+
+
+
+
+
+################### Import assay info
+tbAssays = VTTable.VTTable()
+tbAssays.AddColumn(VTTable.VTColumn('id', 'Text'))
+tbAssays.AddColumn(VTTable.VTColumn('label', 'Text'))
+tbAssays.AddColumn(VTTable.VTColumn('allele1', 'Text'))
+tbAssays.AddColumn(VTTable.VTColumn('allele2', 'Text'))
+tbAssays.AddColumn(VTTable.VTColumn('pf_chr_214', 'Text'))
+tbAssays.AddColumn(VTTable.VTColumn('pf_pos_214', 'Value'))
+tbAssays.AddColumn(VTTable.VTColumn('pf_chr_30', 'Text'))
+tbAssays.AddColumn(VTTable.VTColumn('pf_pos_30', 'Value'))
+tbAssays.AddColumn(VTTable.VTColumn('gene', 'Text'))
+#pf_chr_214 | pf_pos_214 | pf_chr_30 | pf_pos_30 | gene
+LoadSQLTable(tbAssays, 'assay')
+tbAssays.PrintRows(0, 10)
+mapAssays = tbAssays.BuildColDict('id', False)
+tbAssays.saveheadertype = False
+tbAssays.SaveFile(BASEDIR+'/SourceData/datasets/PfReconn/datatables/assays/data')
+
+
+
+################### Import sample x assay info
+tbCallsOrig = VTTable.VTTable()
+tbCallsOrig.AddColumn(VTTable.VTColumn('sample', 'Text'))
+tbCallsOrig.AddColumn(VTTable.VTColumn('assay', 'Text'))
+tbCallsOrig.AddColumn(VTTable.VTColumn('params', 'Value'))
+tbCallsOrig.AddColumn(VTTable.VTColumn('call', 'Text'))
+#pf_chr_214 | pf_pos_214 | pf_chr_30 | pf_pos_30 | gene
+LoadSQLTable(tbCallsOrig, 'stored_vw_snpcalls', None)
+tbCallsOrig.PrintRows(0, 10)
+
+
+tbCallsProcessed = VTTable.VTTable()
+tbCallsProcessed.AddColumn(VTTable.VTColumn('sample_id', 'Text'))
+tbCallsProcessed.AddColumn(VTTable.VTColumn('assay_id', 'Text'))
+tbCallsProcessed.AddColumn(VTTable.VTColumn('assay_label', 'Text'))
+tbCallsProcessed.AddColumn(VTTable.VTColumn('call_default', 'Text'))
+tbCallsProcessed.AddColumn(VTTable.VTColumn('call_manual', 'Text'))
+tbCallsProcessed.AddColumn(VTTable.VTColumn('call_improved', 'Text'))
+tbCallsProcessed.AddColumn(VTTable.VTColumn('call_overall', 'Text'))
+tbCallsProcessed.AddColumn(VTTable.VTColumn('call_illumina', 'Text'))
+
+colnr_sampleid = tbCallsOrig.GetColNr('sample')
+colnr_assayid = tbCallsOrig.GetColNr('assay')
+colnr_params = tbCallsOrig.GetColNr('params')
+colnr_call = tbCallsOrig.GetColNr('call')
+mapCalls = {}
+for rownr in tbCallsOrig.GetRowNrRange():
+    sampleid = tbCallsOrig.GetValue(rownr, colnr_sampleid)
+    assayid = tbCallsOrig.GetValue(rownr, colnr_assayid)
+    param = tbCallsOrig.GetValue(rownr, colnr_params)
+    call = tbCallsOrig.GetValue(rownr, colnr_call)
+    rownr_sample = mapSamples[sampleid]
+    ok = True
+    if assayid not in mapAssays:
+        print('ERROR: Invalid sample id '+str(sampleid))
+        ok = False
+    if ok:
+        rownr_assay = mapAssays[assayid]
+        callid =str(sampleid)+'_'+str(assayid)
+        if callid not in mapCalls:
+            tbCallsProcessed.AddRowEmpty()
+            rownr_call = tbCallsProcessed.GetRowCount()-1
+            tbCallsProcessed.SetValue(rownr_call, 0, sampleid)
+            tbCallsProcessed.SetValue(rownr_call, 1, assayid)
+            tbCallsProcessed.SetValue(rownr_call, 2, tbAssays.GetValue(rownr_assay, tbAssays.GetColNr('label')))
+            mapCalls[callid] = rownr_call
+        rownr_call = mapCalls[callid]
+        if param == 1:
+            tbCallsProcessed.SetValue(rownr_call,3, call)
+        if param == 2:
+            tbCallsProcessed.SetValue(rownr_call,4, call)
+        if param == 3:
+            tbCallsProcessed.SetValue(rownr_call,5, call)
+
+
+# Read & add Illumina calls
+################### Import sample x assay info
+tbCallsIllumina = VTTable.VTTable()
+tbCallsIllumina.AddColumn(VTTable.VTColumn('sample', 'Text'))
+tbCallsIllumina.AddColumn(VTTable.VTColumn('assay', 'Text'))
+tbCallsIllumina.AddColumn(VTTable.VTColumn('illumina_call', 'Text'))
+LoadSQLTable(tbCallsIllumina, 'z_illumina_calls', None)
+tbCallsIllumina.PrintRows(0, 10)
+for rownr in tbCallsIllumina.GetRowNrRange():
+    sampleid = tbCallsIllumina.GetValue(rownr, 0)
+    assayid = tbCallsIllumina.GetValue(rownr, 1)
+    call = tbCallsIllumina.GetValue(rownr, 2)
+    rownr_sample = mapSamples[sampleid]
+    ok = True
+    if assayid not in mapAssays:
+        print('ERROR: Invalid sample id '+str(sampleid))
+        ok = False
+    if ok:
+        rownr_assay = mapAssays[assayid]
+        callid =str(sampleid)+'_'+str(assayid)
+        if callid not in mapCalls:
+            tbCallsProcessed.AddRowEmpty()
+            rownr_call = tbCallsProcessed.GetRowCount()-1
+            tbCallsProcessed.SetValue(rownr_call, 0, sampleid)
+            tbCallsProcessed.SetValue(rownr_call, 1, assayid)
+            tbCallsProcessed.SetValue(rownr_call, 2, tbAssays.GetValue(rownr_assay, tbAssays.GetColNr('label')))
+            mapCalls[callid] = rownr_call
+        rownr_call = mapCalls[callid]
+        tbCallsProcessed.SetValue(rownr_call, 7, call)
+
+
+tbCallsProcessed.PrintRows(0,10)
+tbCallsProcessed.saveheadertype = False
+tbCallsProcessed.SaveFile(BASEDIR+'/SourceData/datasets/PfReconn/datatables/calls/data')
